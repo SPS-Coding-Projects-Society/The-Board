069fa32a9c634d7c94ecc311e2ca7796855f5569
Kepler45 <46714055+Kepler45@users.noreply.github.com>
2020-11-25 12:13:16 +0000

    Add Streampage & carousel

--- the_board/lib/pages/streamPage.dart
+++ the_board/lib/pages/streamPage.dart
@@ -1,14 +1,115 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
-class StreamPage extends StatelessWidget {
-  @override
+
+class Item1 extends StatelessWidget {
+  const Item1({Key key}) : super(key: key);
+@override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        height: double.infinity,
-        color: Colors.blue,
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          stops: [0.3, 1],
+          colors: [Color(0xffff4000),Color(0xffffcc66),]
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "Data",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 22.0,
+              fontWeight: FontWeight.bold
+            )
+          ),
+          Text(
+            "Data",
+            style: TextStyle(
+              color: Colors.redAccent,
+              fontSize: 17.0,
+              fontWeight: FontWeight.w600
+            )
+          ),
+        ],
       ),
     );
   }
+}
+
+class StreamPage extends StatelessWidget {
+  @override
+  
+
+  List getCardList()
+  {
+    List myCardList = [Item1()];
+
+    
+
+    return myCardList;
+  }
+
+  List<T> map<T>(List list, Function handler) {
+    List<T> result = [];
+    for (var i = 0; i < list.length; i++) {
+      result.add(handler(i, list[i]));
+    }
+    return result;
+  }
+
+  Widget build(BuildContext context) {
+    List myCardList = getCardList();
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          CarouselSlider(options: CarouselOptions(
+                height: 200.0,
+                autoPlay: true,
+                autoPlayInterval: Duration(seconds: 3),
+                autoPlayAnimationDuration: Duration(milliseconds: 800),
+                autoPlayCurve: Curves.fastOutSlowIn,
+                pauseAutoPlayOnTouch: true,
+                aspectRatio: 2.0,
+                // onPageChanged: (index, reason) {
+                //   setState(() {
+                //     _currentIndex = index;
+                //   });
+                // },
+              ),
+                items: myCardList.map((card){
+                return Builder(
+                  builder:(BuildContext context){
+                    return Container(
+                      height: MediaQuery.of(context).size.height*0.30,
+                      width: MediaQuery.of(context).size.width,
+                      child: Card(
+                        color: Colors.blueAccent,
+                        child: card,
+                      ),
+                    );
+                  }
+                );
+              }).toList(),
+          ),
+          Row(
+      children: <Widget>[
+        Text("data")
+      ],
+    )
+        ],
+        
+      ),
+
+
+
+    );
+  }
+
+  
+
 }
